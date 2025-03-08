f6d733e5c9fdceb558acf7737d7537d70a57c9bf
VonRage <134968282+VonRage@users.noreply.github.com>
2025-03-08 15:53:19 -0500

    Player movement changes and title screen
    Combined the github Celeste movement in godot's code with mine for better feeling movement. Head be wonky tho. Scenes on title "switch" but old one stays around.

--- title-screen/Title.gd
+++ title-screen/Title.gd
@@ -1,20 +1,20 @@
 extends Node2D
 
+var level_instance = "res://title-screen/Title.tscn"
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+func unload_level():
+	if (is_instance_valid(level_instance)):
+		#level_instance.queue_free()
+		#level_instance = null
+		print_debug("work")
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
+func load_level(level_name : String):
+	unload_level()
+	var level_path := "res://world-1/%s.tscn" % level_name
+	var level_resource := load(level_path)
+	if (level_resource):
+		level_instance = level_resource.instance()
+		self.add_child(level_instance)
 
 func _on_Button_pressed():
-	pass
+	load_level("Castle")
