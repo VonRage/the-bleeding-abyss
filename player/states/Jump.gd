c5a014448e8f6c8bc770008cdebfd410a8e43a5b
VonRage <tylervonroth@gmail.com>
2025-04-09 11:58:55 -0400

    Jump Height
    Changed jump state to lerp instead of using a preset var. Still janky but better.

--- player/states/Jump.gd
+++ player/states/Jump.gd
@@ -1,7 +1,8 @@
 extends State
 
-export (float) var jump_force = 1000
-export (float) var min_jump_height = 200
+export (float) var jump_force = 25
+export (float) var min_jump_height = 300
+export (float) var max_jump_height = 1000
 export (float) var max_speed = 300
 export (float) var acceleration = 60
 
@@ -15,37 +16,51 @@ onready var walk_state: State = get_node(walk_node)
 onready var idle_state: State = get_node(idle_node)
 onready var throw_state: State = get_node(throw_node)
 
+var canJump : bool = true
+var jump_time : float = 0.0
+
 
 func enter() -> void:
 	# This calls the base class enter function, which is necessary here
 	# to make sure the animation switches
 	.enter()
-	if Input.is_action_just_pressed("ui_jump"):
-		parent.velocity.y = -jump_force
+	parent.velocity.y = 0
+	canJump = true
 
 func process_physics(delta: float) -> State:
 	var direction = 0
+	if Input.is_action_pressed("ui_jump"):
+		if canJump == true:
+			if parent.velocity.y > -max_jump_height:
+				parent.velocity.y = lerp(-min_jump_height, -max_jump_height, jump_time)
+				jump_time += delta
+			elif parent.velocity.y <= -max_jump_height:
+				canJump = false
+	if Input.is_action_just_released("ui_jump"):
+		canJump = false
 	if Input.is_action_pressed("ui_left"):
 		direction = -1
 		parent.body_anim.flip_h = true
 	elif Input.is_action_pressed("ui_right"):
 		direction = 1
 		parent.body_anim.flip_h = false
+	else:
+		direction = 0
 	if Input.is_action_just_pressed("ui_throw"):
 		return throw_state
 	
-	print_debug(parent.velocity.y)
 	parent.velocity.x = clamp(parent.velocity.x + (direction * acceleration), -max_speed, max_speed)
-	parent.velocity.y += parent.gravity
 	parent.velocity = parent.move_and_slide(parent.velocity, Vector2.UP)
 	
-	if parent.velocity.y > -100:
+	if canJump == false:
 		return fall_state
 
 	if parent.is_on_floor():
 		if direction != 0:
-			#if Input.is_action_pressed("run"):
-			#	return run_state
 			return walk_state
 		return idle_state
 	return null
+
+
+func exit():
+	jump_time = 0.0
